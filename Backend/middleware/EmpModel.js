1b20b6fcd181d856b4a3a3c465d1d9b494749e61
josh120604 <joshuabihute@gmail.com>
2025-10-24 09:52:14 +0200

    added emps items

---
+++ Backend/middleware/EmpModel.js
@@ -0,0 +1,32 @@
+import {pool} from "../config/db.js"
+
+
+export const UpdateEmp = async (employeeData) => {
+  try {
+    await pool.query(
+      `UPDATE tracker_db.employees 
+       SET last_name = ?, 
+           contact_no = ?, 
+           email = ?, 
+           address = ?, 
+           id = ?, 
+           employment_status = ?, 
+           leave_balance = ? 
+       WHERE employee_id = ?`,
+      [
+        employeeData.last_name,
+        employeeData.contact_no,
+        employeeData.email,
+        employeeData.address,
+        employeeData.id,
+        employeeData.employment_status,
+        employeeData.leave_balance,
+        employeeData.employee_id
+      ]
+    );
+    return { success: true };
+  } catch (error) {
+    console.error("Database error:", error);
+    throw error;
+  }
+};
