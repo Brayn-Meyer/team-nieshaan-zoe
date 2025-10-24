39268f5a173a5603194fb1780b9e60b96fefad1f
Brayn-Meyer <coolmrepic9876@gmail.com>
2025-10-24 12:14:36 +0200

    added routes to servers

--- Backend/server.js
+++ Backend/server.js
@@ -17,6 +17,25 @@ app.use(express.json());
 app.get('/filter', getfilterCon);
 app.get('/filterAll', getfilterAllCon);
 
+import { getTotalEmployeesData, getTotalCheckedInData, getTotalCheckedOutDataCon, getTotalAbsentDataCon } from '../controllers/admin_cards_con'
+import { config } from 'dotenv'
+
+app.get("/totalEmployees", getTotalEmployeesData)
+app.get("/checkedIn", getTotalCheckedInData)
+app.get("/checkedOut", getTotalCheckedOutDataCon)
+app.get("/absent", getTotalAbsentDataCon)
+
+import { getClockInOutDataCon } from '../controllers/clock_in_out_con'
+
+app.get("/clockInOut", getClockInOutDataCon)
+
+import { addEmployeeCon } from '../controllers/employeesCon.js';
+import { deleteEmployeeCon } from '../controllers/employeesCon.js';
+
+// route to add a new employee
+router.post('/addEmployee', addEmployeeCon);
+router.delete('/removeEmployee/:id', deleteEmployeeCon);
+
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
