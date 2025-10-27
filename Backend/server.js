3af4059056005b05b6504394300e918f8ab6243d
luvogangca-7 <luvogangca@gmail.com>
2025-10-27 14:33:54 +0200

    updated server.js (connected sockets to the server.)

--- Backend/server.js
+++ Backend/server.js
@@ -1,51 +1,58 @@
 import express from 'express';
 import { config } from 'dotenv';
 import cors from 'cors';
-
-import { addEmployeeCon, deleteEmployeeCon } from './controllers/employeesCon.js';
-import { getfilterAllCon } from './controllers/filterAllCon.js';
-import { getfilterCon } from './controllers/filterCon.js';
-// import { editEmployeeRoutes } from './routes/EditEmployeeCon.js';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 import { getClockInOutDataCon } from './controllers/clock_in_out_con.js';
-import {
-  getTotalEmployeesDataCon,
-  getTotalCheckedInDataCon,
-  getTotalCheckedOutDataCon,
-  getTotalAbsentDataCon,
-} from './controllers/admin_cards_con.js';
+import { getTotalCheckedOutDataCon, getTotalAbsentDataCon, getTotalEmployeesDataCon, getTotalCheckedInDataCon } from './controllers/admin_cards_con.js';
+import { addEmployeeCon, deleteEmployeeCon } from './controllers/employeesCon.js';
+import { EditEmpCon } from './controllers/EditEmployeeCon.js';
+import { getfilterAllCon } from './controllers/filterAllCon.js';
+import { getfilterCon } from './controllers/filterCon.js';
 
 config();
 
 const app = express();
 const PORT = process.env.PORT || 9090;
 
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {origin: '*'}
+});
+
 app.use(cors());
 app.use(express.json());
 
-// ===== Routes =====
+app.set('io', io);
 
-// Filters
 app.get('/filter', getfilterCon);
 app.get('/filterAll', getfilterAllCon);
-// app.use('/employee', editEmployeeRoutes);
 
 
 
-// Admin Cards
-app.get('/totalEmployees', getTotalEmployeesDataCon);
-app.get('/checkedIn', getTotalCheckedInDataCon);
-app.get('/checkedOut', getTotalCheckedOutDataCon);
-app.get('/absent', getTotalAbsentDataCon);
+app.get("/totalEmployees", getTotalEmployeesDataCon)
+app.get("/checkedIn", getTotalCheckedInDataCon)
+app.get("/checkedOut", getTotalCheckedOutDataCon)
+app.get("/absent", getTotalAbsentDataCon)
 
-// Clock In/Out
-app.get('/clockInOut', getClockInOutDataCon);
 
-// Employees
+app.get("/clockInOut", getClockInOutDataCon)
+
 app.post('/addEmployee', addEmployeeCon);
 app.delete('/removeEmployee/:id', deleteEmployeeCon);
+app.put('/editEmployee/:employee_id', EditEmpCon);
 
-// ===== Start Server =====
-app.listen(PORT, () => {
-  console.log(`✅ Server running at http://localhost:${PORT}`);
+io.on('connection', (socket) => {
+
+  console.log('User connected:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
 });
