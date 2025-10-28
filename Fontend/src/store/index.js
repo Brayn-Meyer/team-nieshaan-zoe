a1e16e99cdd15ad08114f87b5c76d60be45d80dc
Brayn-Meyer <coolmrepic9876@gmail.com>
2025-10-28 11:07:36 +0200

    started intergration

--- Fontend/src/store/index.js
+++ Fontend/src/store/index.js
@@ -1,13 +1,34 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+const API_URL = "http://localhost:3315/"
 
 export default createStore({
   state: {
+    employee_info: []
   },
   getters: {
   },
   mutations: {
+    get_employee_info(state, payload) {
+      state.employee_info = payload;
+    },
   },
   actions: {
+    async fetch_employee_info({ commit }) {
+      let data = await axios.get(`${API_URL}clockInOut`)
+      commit('get_employee_info', data.data.employees)
+      console.log(data.data.employees)
+    },
+
+    async add_employee({ dispatch }, payload) {
+      await axios.post(`${API_URL}employees`, payload)
+      dispatch("fetch_employee_info")
+    },
+
+    async edit_employee({ dispatch }, payload){
+      await axios.patch(`${API_URL}employees`, payload)
+      dispatch("fetch_employee_info")
+    },
   },
   modules: {
   }
