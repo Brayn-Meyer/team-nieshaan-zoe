57f3c284662727609b94e152c9c92499518df2f4
AaliyahVer <aaliyahvermeulen1001@gmail.com>
2025-10-29 12:35:54 +0200

    Time log added

---
+++ Fontend/src/views/Timelog.View.vue
@@ -0,0 +1,23 @@
+<template>
+<NavComp/>
+<br><br><br><br><br><br><br>
+<TimelogFilters/><br>
+<TimeLog/>
+</template>
+<script>
+import TimeLog from '@/components/TimeLog.vue';
+import NavComp from '@/components/NavComp.vue'
+import TimelogFilters from '@/components/TimelogFilters.vue';
+
+export default {
+  components: {
+    TimeLog,
+  	NavComp,
+    TimelogFilters
+	},
+};
+</script>
+
+<style scoped>
+
+</style>
